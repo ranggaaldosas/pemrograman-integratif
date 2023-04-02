a7d47a350d31e7c1c878239abd9168350357a0c2
rangga <ranggaaldosas@gmail.com>
2023-04-02 23:36:42 +0700

    add comments

--- gRPC_API-telebot-master/client.py
+++ gRPC_API-telebot-master/client.py
@@ -9,43 +9,55 @@ bot = telegram.Bot(token='YOUR_TELEBOT_TOKEN')
 updater = Updater(token='YOUR_TELEBOT_TOKEN', use_context=True)
 dispatcher = updater.dispatcher
 
+# Inisialisasi client untuk mengakses TodoService
 import grpc
 import todo_pb2
 import todo_pb2_grpc
 
-
 class TodoServiceClient:
 
     def __init__(self, host='localhost', port=50051):
+        # Membuat insecure channel ke TodoService
         self.channel = grpc.insecure_channel(f"{host}:{port}")
         self.stub = todo_pb2_grpc.TodoServiceStub(self.channel)
 
     def Create(self, title):
+        # Membuat request untuk operasi Create
         request = todo_pb2.CreateRequest(title=title)
+        # Memanggil RPC Create dari TodoService
         return self.stub.Create(request)
 
     def Read(self, id):
+        # Membuat request untuk operasi Read
         request = todo_pb2.ReadRequest(id=id)
+        # Memanggil RPC Read dari TodoService
         return self.stub.Read(request)
 
     def Update(self, id, title):
+        # Membuat request untuk operasi Update
         request = todo_pb2.UpdateRequest(id=id, title=title)
+        # Memanggil RPC Update dari TodoService
         return self.stub.Update(request)
 
     def Delete(self, id):
+        # Membuat request untuk operasi Delete
         request = todo_pb2.DeleteRequest(id=id)
+        # Memanggil RPC Delete dari TodoService
         return self.stub.Delete(request)
 
     def List(self):
+        # Membuat request untuk operasi List
         request = todo_pb2.ListRequest()
+        # Memanggil RPC List dari TodoService
         return self.stub.List(request)
 
-
+# Membuat instance TodoServiceClient dan menyimpannya ke dalam bot_data
 todo_service = TodoServiceClient('localhost:50051')
 dispatcher.bot_data['todo_service'] = todo_service
 
-
+# Menambahkan command-handler ke dalam bot
 def start(update, context):
+    # Menampilkan pesan untuk command /start
     message = "Halo! Gunakan daftar command berikut untuk menggunakan Aplikasi\n\n"
     message += "Berikut adalah daftar command yang tersedia:\n"
     message += "/add <judul todo> - untuk menambahkan todo list\n"
@@ -53,78 +65,79 @@ def start(update, context):
     message += "/update <id todo> <judul baru> - untuk mengubah todo list\n"
     message += "/delete <id todo> - untuk menghapus todo list\n"
     message += "/list - untuk melihat semua todo list"
+    # Mengirim pesan ke chat
     context.bot.send_message(chat_id=update.effective_chat.id, text=message)
 
-
 def add(update, context):
+    # Mendapatkan judul dari command argument
     title = context.args[0]
-    with grpc.insecure_channel('localhost:50051') as channel:
-        stub = todo_pb2_grpc.TodoServiceStub(channel)
-        response = stub.Create(todo_pb2.CreateRequest(title=title))
+    # Memanggil RPC Create dari TodoService menggunakan insecure channel
+    response = todo_service.Create(title)
+    # Mengirim pesan ke chat
     context.bot.send_message(chat_id=update.effective_chat.id,
                              text=response.message)
 
-
 def read(update, context):
+    # Mendapatkan ID dari command argument
     id = context.args[0]
     try:
-        with grpc.insecure_channel('localhost:50051') as channel:
-            stub = todo_pb2_grpc.TodoServiceStub(channel)
-            response = stub.Read(todo_pb2.ReadRequest(id=id))
-            context.bot.send_message(chat_id=update.effective_chat.id,
-                                     text=response.todo.title)
+        # Memanggil RPC Read dari TodoService menggunakan insecure channel
+        response = todo_service.Read(id)
+        # Mengirim pesan ke chat
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                 text=response.todo.title)
     except grpc.RpcError as e:
         context.bot.send_message(chat_id=update.effective_chat.id, text=str(e))
 
-
 def update(update, context):
+    # Mendapatkan ID dan judul baru dari command argument
     id = context.args[0]
     title = context.args[1]
-    with grpc.insecure_channel('localhost:50051') as channel:
-        stub = todo_pb2_grpc.TodoServiceStub(channel)
-        try:
-            response = stub.Update(todo_pb2.UpdateRequest(id=id, title=title))
-            context.bot.send_message(chat_id=update.effective_chat.id,
-                                     text=response.message)
-        except grpc.RpcError as e:
-            context.bot.send_message(chat_id=update.effective_chat.id,
-                                     text=str(e))
-
+    # Memanggil RPC Update dari TodoService menggunakan insecure channel
+    try:
+        response = todo_service.Update(id, title)
+        # Mengirim pesan ke chat
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                 text=response.message)
+    except grpc.RpcError as e:
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                 text=str(e))
 
 def delete(update, context):
+    # Mendapatkan ID dari command argument
     id = context.args[0]
-    with grpc.insecure_channel('localhost:50051') as channel:
-        stub = todo_pb2_grpc.TodoServiceStub(channel)
-        try:
-            response = stub.Delete(todo_pb2.DeleteRequest(id=id))
-            context.bot.send_message(chat_id=update.effective_chat.id,
-                                     text=response.message)
-        except grpc.RpcError as e:
-            context.bot.send_message(chat_id=update.effective_chat.id,
-                                     text=str(e))
-
+    # Memanggil RPC Delete dari TodoService menggunakan insecure channel
+    try:
+        response = todo_service.Delete(id)
+        # Mengirim pesan ke chat
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                 text=response.message)
+    except grpc.RpcError as e:
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                 text=str(e))
 
 def list(update, context):
     try:
-        with grpc.insecure_channel('localhost:50051') as channel:
-            stub = todo_pb2_grpc.TodoServiceStub(channel)
-            response = stub.List(todo_pb2.ListRequest())
-            todo_list = response.todos
-            if not todo_list:
-                context.bot.send_message(chat_id=update.effective_chat.id,
-                                         text="Tidak ada todo list")
-                return
-            message = 'Todo list:\n'
-            for todo in todo_list:
-                message += f"- {todo.title} (ID: {todo.id})\n"
+        # Memanggil RPC List dari TodoService menggunakan insecure channel
+        response = todo_service.List()
+        todo_list = response.todos
+        if not todo_list:
+            # Mengirim pesan ke chat jika tidak ada todo list
             context.bot.send_message(chat_id=update.effective_chat.id,
-                                     text=message)
+                                     text="Tidak ada todo list")
+            return
+        # Mengirim pesan ke chat dengan daftar todo list
+        message = 'Todo list:\n'
+        for todo in todo_list:
+            message += f"- {todo.title} (ID: {todo.id})\n"
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                 text=message)
     except Exception as e:
         print(e)
         context.bot.send_message(chat_id=update.effective_chat.id,
                                  text="Gagal mengambil todo list")
 
-
+# Menambahkan command-handler ke dalam bot
 start_handler = CommandHandler('start', start)
 add_handler = CommandHandler('add', add)
 read_handler = CommandHandler('read', read)
@@ -138,4 +151,5 @@ dispatcher.add_handler(update_handler)
 dispatcher.add_handler(delete_handler)
 dispatcher.add_handler(list_handler)
 
+# Memulai polling untuk telegram bot
 updater.start_polling()
--- gRPC_API-telebot-master/server.py
+++ gRPC_API-telebot-master/server.py
@@ -1,3 +1,4 @@
+# Import modul-modul yang diperlukan
 import grpc
 import firebase_admin
 from firebase_admin import credentials, firestore
@@ -5,71 +6,104 @@ import todo_pb2
 import todo_pb2_grpc
 from concurrent import futures
 
-# Inisialisasi firebase
+# Inisialisasi firebase menggunakan service account key
 cred = credentials.Certificate('./serviceAccountKey.json')
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
-
+# Membuat class TodoService yang mewarisi todo_pb2_grpc.TodoServiceServicer
 class TodoService(todo_pb2_grpc.TodoServiceServicer):
 
+    # Implementasi RPC Create
     def Create(self, request, context):
+        # Membuat reference ke document baru dalam collection 'todo'
         todo_ref = db.collection('todo').document()
+        # Menyimpan data baru dalam document tersebut dengan field title dari request
         todo_ref.set({'title': request.title})
+        # Mengembalikan response yang berisi pesan 'Todo created'
         return todo_pb2.CreateResponse(message='Todo created')
 
+    # Implementasi RPC Read
     def Read(self, request, context):
         try:
+            # Mencari document dengan id yang diberikan dalam request
             todo_ref = db.collection('todo').document(request.id).get()
+            # Jika document tidak ditemukan, membatalkan permintaan dengan error NOT_FOUND
             todo = todo_ref.to_dict()
             if not todo:
                 context.abort(grpc.StatusCode.NOT_FOUND, "Todo not found")
+            # Mengembalikan response yang berisi informasi mengenai todo item yang ditemukan
             return todo_pb2.ReadResponse(
                 todo=todo_pb2.Todo(id=request.id, title=todo['title']))
+        # Menangkap error yang terjadi dan membatalkan permintaan dengan error INTERNAL
         except Exception as e:
             context.abort(grpc.StatusCode.INTERNAL, str(e))
 
+    # Implementasi RPC Update
     def Update(self, request, context):
+        # Mencari document dengan id yang diberikan dalam request
         todo_ref = db.collection('todo').document(request.id)
+        # Jika document ditemukan, memperbarui field title dengan value dari request
         if todo_ref.get().exists:
             todo_ref.update({'title': request.title})
+            # Mengembalikan response yang berisi pesan 'Todo updated'
             return todo_pb2.UpdateResponse(message='Todo updated')
+        # Jika document tidak ditemukan, membatalkan permintaan dengan error NOT_FOUND
         else:
             context.set_details('Todo not found')
             context.set_code(grpc.StatusCode.NOT_FOUND)
             return todo_pb2.UpdateResponse()
 
+    # Implementasi RPC Delete
     def Delete(self, request, context):
+        # Mencari document dengan id yang diberikan dalam request
         todo_ref = db.collection('todo').document(request.id)
+        # Jika document ditemukan, menghapus document tersebut
         if todo_ref.get().exists:
             todo_ref.delete()
+            # Mengembalikan response yang berisi pesan 'Todo deleted'
             return todo_pb2.DeleteResponse(message='Todo deleted')
+        # Jika document tidak ditemukan, membatalkan permintaan dengan error NOT_FOUND
         else:
             context.set_details('Todo not found')
             context.set_code(grpc.StatusCode.NOT_FOUND)
             return todo_pb2.DeleteResponse()
 
+    # Implementasi RPC List
     def List(self, request, context):
         try:
             todos = []
+            # Mendapatkan seluruh document dalam collection 'todo'
             for todo in db.collection('todo').get():
+                # Mengubah setiap document menjadi dictionary
                 todo_dict = todo.to_dict()
+                # Menambahkan field 'id' ke dictionary yang nilainya adalah id dari document
                 todo_dict['id'] = todo.id
+                # Menambahkan dictionary tersebut ke dalam list todos
                 todos.append(todo_dict)
+            # Mengembalikan response yang berisi list todos
             return todo_pb2.ListResponse(todos=todos)
+        # Menangkap error yang terjadi dan membatalkan permintaan dengan error INTERNAL
         except Exception as e:
             context.set_details(str(e))
             context.set_code(grpc.StatusCode.INTERNAL)
             return todo_pb2.ListResponse()
 
 
+# Function untuk menjalankan server
 def serve():
+    # Membuat server dengan 10 worker untuk melayani request
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    # Menambahkan TodoService ke dalam server
     todo_pb2_grpc.add_TodoServiceServicer_to_server(TodoService(), server)
+    # Mengaktifkan server pada port 50051
     server.add_insecure_port('[::]:50051')
+    # Menjalankan server
     server.start()
+    # Menunggu permintaan dari client
     server.wait_for_termination()
 
 
 if __name__ == '__main__':
+    # Jalanin server
     serve()
